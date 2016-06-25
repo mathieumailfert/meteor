1aa8aee25dbec7dd29e72e8612128c0d34cc7e4d
Mathieu Mailfert <mathieumailfert@macbook-pro-de-mathieu-2.local>
2016-06-25 18:44:41 +0200

    Initial Commit

---
+++ Meteor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meteor
+//
+//  Created by Mathieu Mailfert on 25/06/2016.
+//  Copyright © 2016 Mathieu Mailfert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
