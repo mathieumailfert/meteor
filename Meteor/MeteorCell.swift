d34a5e2fc589251f7bad230429e80332bed24524
Mathieu Mailfert <mathieumailfert@MacBook-Pro-de-Mathieu-2.local>
2016-06-28 18:14:03 +0200

    first commit

---
+++ Meteor/MeteorCell.swift
@@ -0,0 +1,13 @@
+//
+//  MeteorCell.swift
+//  Meteor
+//
+//  Created by Mathieu Mailfert on 25/06/2016.
+//  Copyright © 2016 Mathieu Mailfert. All rights reserved.
+//
+
+import Cocoa
+
+class MeteorCell: UITableViewCell {
+
+}
